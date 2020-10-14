0859e6c4619243977849fe28263d652d88d71c88
Sean LaFlam <sean.laflam@gmail.com>
2020-10-13 21:26:29 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data['results'][0]
+    console.log(obj)
+    const nameField = document.getElementById('fullname')
+    nameField.textContent = obj.name.first +" " + obj.name.last
+  })
 });
